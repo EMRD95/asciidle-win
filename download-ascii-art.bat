623551fe7b062381b6ac6a8cf0bc088f979200f9
EMRD95 <114953576+EMRD95@users.noreply.github.com>
2023-04-10 16:17:50 +0000

    Update download-ascii-art.bat

--- download-ascii-art.bat
+++ download-ascii-art.bat
@@ -1,8 +1,13 @@
 @echo off
-echo Downloading Pinups...
-powershell -Command "Invoke-WebRequest -Uri 'https://files.catbox.moe/ykkxlt.zip' -OutFile 'Pinups.zip'"
-echo Extracting Pinups...
-powershell -Command "Expand-Archive -Path 'Pinups.zip' -DestinationPath '.'"
-echo Deleting the zip file...
-del Pinups.zip
-echo Done!
+set "txt_folder=txt"
+if exist "%txt_folder%\pinups$$.txt" (
+    echo Pinups already exist. Skipping download and extraction.
+) else (
+    echo Downloading Pinups...
+    powershell -Command "Invoke-WebRequest -Uri 'https://files.catbox.moe/ykkxlt.zip' -OutFile 'Pinups.zip'"
+    echo Extracting Pinups...
+    powershell -Command "Expand-Archive -Path 'Pinups.zip' -DestinationPath '.'"
+    echo Deleting the zip file...
+    del Pinups.zip
+    echo Done!
+)
